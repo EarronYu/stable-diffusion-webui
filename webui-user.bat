72542a521b6a2ea69e83efe761e0726b9838821f
OzawaYilang <69912395+OzawaYilang@users.noreply.github.com>
2022-11-04 22:19:59 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@ pause
 
 @echo off
 
-set PYTHON= "C:\Users\x7498\anaconda3\envs\py310\python.exe"
+set PYTHON= "C:\Users\x7498\anaconda3\envs\stable-diffusion-webui\python.exe"
 set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
