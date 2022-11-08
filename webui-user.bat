aea39cbab37a8195804027f4f93174743db0da05
OzawaYilang <69912395+OzawaYilang@users.noreply.github.com>
2022-11-08 10:04:46 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -6,6 +6,6 @@ pause
 set PYTHON= "C:\Users\x7498\anaconda3\envs\stable-diffusion-webui\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--disable-safe-unpickle
 
 call webui.bat
